5583d79524909191f38d07259cb2fe94c3739088
Gopi Melam <gmelam@udel.edu>
2026-05-09 14:54:12 -0400

    Index created for all images to import easily and used pokemon.json for names

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,5 @@
-import bulbasaurImage from "../assets/images/0001.png";
+import images from "../assets/images";
+import pokemonData from "../assets/datasets/pokemon.json"
 
 export interface Pokemon {
 	dexNumber: number;
@@ -8,19 +9,24 @@ export interface Pokemon {
 }
 
 function Card() {
-	const bulbasaur: Pokemon = {
-		dexNumber: 1,
-		name: "Bulbasaur",
-		image: bulbasaurImage,
-		collected: false,
-	};
+	const pokemonList: Pokemon[] = [];
+	for (let i = 1; i <= 1025; i++) {
+		pokemonList.push({
+			dexNumber: i,
+			name: pokemonData.pokemon[i.toString()].N.charAt(0).toUpperCase() + pokemonData.pokemon[i.toString()].N.slice(1),
+			image: i.toString().padStart(4, "0"),
+			collected: false,
+		});
+	}
+
+	const currentIndex: number = 1024;
 
 	return (
 		<div>
-			<h2>{bulbasaur.name}</h2>
-			<h2>{bulbasaur.dexNumber}</h2>
-			<h2>{bulbasaur.collected ? "Collected" : "Not Collected"}</h2>
-			<img src={bulbasaur.image} alt={bulbasaur.name} />
+			<h2>{pokemonList[currentIndex - 1].name}</h2>
+			<h2>{pokemonList[currentIndex - 1].dexNumber}</h2>
+			<h2>{pokemonList[currentIndex - 1].collected ? "Collected" : "Not Collected"}</h2>
+			<img src={images[pokemonList[currentIndex - 1].image]} alt={pokemonList[currentIndex].name} />
 		</div>
 	);
 }

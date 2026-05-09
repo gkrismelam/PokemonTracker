be0b9535d0bb509ae4bf62ae67b2c89c7e3fcda2
Gopi Melam <gmelam@udel.edu>
2026-05-09 17:27:05 -0400

    Seperated headers by generation

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -15,7 +15,72 @@ function Card( {pokemonList, setPokemonList}: CardProps ) {
 
 	return (
 		<div className="card-organizer">
-			{pokemonList.map((pokemon) => (
+			<h1>Generation 1: Kanto</h1>
+			{pokemonList.slice(0, 151).map((pokemon) => (
+				<div className={`card ${pokemon.collected ? "collected" : "not-collected"}`} key={pokemon.dexNumber} onClick={() => changeCollected(pokemon.dexNumber)}>
+					<h2>{pokemon.dexNumber}</h2>
+					<h2>{pokemon.name}</h2>
+					<img className="pokemon-image" src={images[pokemon.image]} alt={pokemon.name} />
+				</div>
+			))}
+			<h1>Generation 2: Johto</h1>
+			{pokemonList.slice(151, 251).map((pokemon) => (
+				<div className={`card ${pokemon.collected ? "collected" : "not-collected"}`} key={pokemon.dexNumber} onClick={() => changeCollected(pokemon.dexNumber)}>
+					<h2>{pokemon.dexNumber}</h2>
+					<h2>{pokemon.name}</h2>
+					<img className="pokemon-image" src={images[pokemon.image]} alt={pokemon.name} />
+				</div>
+			))}
+			<h1>Generation 3: Hoenn</h1>
+			{pokemonList.slice(251, 386).map((pokemon) => (
+				<div className={`card ${pokemon.collected ? "collected" : "not-collected"}`} key={pokemon.dexNumber} onClick={() => changeCollected(pokemon.dexNumber)}>
+					<h2>{pokemon.dexNumber}</h2>
+					<h2>{pokemon.name}</h2>
+					<img className="pokemon-image" src={images[pokemon.image]} alt={pokemon.name} />
+				</div>
+			))}
+			<h1>Generation 4: Sinnoh</h1>
+			{pokemonList.slice(386, 493).map((pokemon) => (
+				<div className={`card ${pokemon.collected ? "collected" : "not-collected"}`} key={pokemon.dexNumber} onClick={() => changeCollected(pokemon.dexNumber)}>
+					<h2>{pokemon.dexNumber}</h2>
+					<h2>{pokemon.name}</h2>
+					<img className="pokemon-image" src={images[pokemon.image]} alt={pokemon.name} />
+				</div>
+			))}
+			<h1>Generation 5: Unova</h1>
+			{pokemonList.slice(493, 649).map((pokemon) => (
+				<div className={`card ${pokemon.collected ? "collected" : "not-collected"}`} key={pokemon.dexNumber} onClick={() => changeCollected(pokemon.dexNumber)}>
+					<h2>{pokemon.dexNumber}</h2>
+					<h2>{pokemon.name}</h2>
+					<img className="pokemon-image" src={images[pokemon.image]} alt={pokemon.name} />
+				</div>
+			))}
+			<h1>Generation 6: Kalos</h1>
+			{pokemonList.slice(649, 721).map((pokemon) => (
+				<div className={`card ${pokemon.collected ? "collected" : "not-collected"}`} key={pokemon.dexNumber} onClick={() => changeCollected(pokemon.dexNumber)}>
+					<h2>{pokemon.dexNumber}</h2>
+					<h2>{pokemon.name}</h2>
+					<img className="pokemon-image" src={images[pokemon.image]} alt={pokemon.name} />
+				</div>
+			))}
+			<h1>Generation 7: Alola</h1>
+			{pokemonList.slice(721, 809).map((pokemon) => (
+				<div className={`card ${pokemon.collected ? "collected" : "not-collected"}`} key={pokemon.dexNumber} onClick={() => changeCollected(pokemon.dexNumber)}>
+					<h2>{pokemon.dexNumber}</h2>
+					<h2>{pokemon.name}</h2>
+					<img className="pokemon-image" src={images[pokemon.image]} alt={pokemon.name} />
+				</div>
+			))}
+			<h1>Generation 8: Galar</h1>
+			{pokemonList.slice(809, 905).map((pokemon) => (
+				<div className={`card ${pokemon.collected ? "collected" : "not-collected"}`} key={pokemon.dexNumber} onClick={() => changeCollected(pokemon.dexNumber)}>
+					<h2>{pokemon.dexNumber}</h2>
+					<h2>{pokemon.name}</h2>
+					<img className="pokemon-image" src={images[pokemon.image]} alt={pokemon.name} />
+				</div>
+			))}
+			<h1>Generation 9: Paldea</h1>
+			{pokemonList.slice(905, 1025).map((pokemon) => (
 				<div className={`card ${pokemon.collected ? "collected" : "not-collected"}`} key={pokemon.dexNumber} onClick={() => changeCollected(pokemon.dexNumber)}>
 					<h2>{pokemon.dexNumber}</h2>
 					<h2>{pokemon.name}</h2>

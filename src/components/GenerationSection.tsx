5d5f4bd8f77af96aea2cd8df1c50b7a5475226ee
Gopi Melam <gmelam@udel.edu>
2026-05-15 11:11:54 -0400

    Imported new images which are more compressed and created new aggregation index file renaming old ones

--- src/components/GenerationSection.tsx
+++ src/components/GenerationSection.tsx
@@ -1,4 +1,4 @@
-import images from "../assets/images";
+import images from "../assets/images/index";
 import type { GenerationSectionProps } from "../utils/props";
 import "../css/GenerationSection.css"
 
